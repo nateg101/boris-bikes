2c2fa12074011fbdac1b198f34cee24ac5497960
Melvin Lau <lau.melvin@icloud.com>
2019-09-18 17:38:24 +0100

    End of Step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
+  def initialize
+    @bikes = []
+  end
   def release_bike
-    fail("No bikes available") unless @bike
-    @bike
+    fail("No bikes available") if @bikes.empty?
+    @bikes
   end
   def dock(bike)
-    fail("No space available") if @bike
-    @bike = bike
+    fail("No space available") if @bikes.count >= 20
+    @bikes << bike
   end
-attr_reader :bike
+attr_reader :bikes
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,37 +9,40 @@ describe DockingStation do
   it "releases working bikes" do
     bike = Bike.new
     subject.dock(bike)
-    bike = subject.release_bike
+    bike = subject.release_bike.last
     expect(bike).to be_working
   end
 
+  # Testing the :dock method
   describe "#dock()" do
     it "docks a bike and confirms by returning the bike reference" do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike).last).to eq bike
     end
 
     it "raises an error when full capacity" do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error "No space available"
     end
   end
 
+  # Test accessing the @bike instance variable
   it "returns bike when looking for a docked bike" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes.last).to eq bike
   end
 
-
+  # Testing the :release_bike method
   describe '#release_bike' do
     it "releases a bike" do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      expect(subject.release_bike.last).to eq bike
     end
     it "raises an error when there are no bikes" do
       expect { subject.release_bike }.to raise_error "No bikes available"
     end
   end
+
 end
